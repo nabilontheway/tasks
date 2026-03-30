20a22b86609a6cf74e843a33a673f8dbefe8f961
Nabil Ahmed <risingsun@tutamail.com>
2026-03-30 10:23:36 +0600

    Build tool : New preset

---
+++ bin/CMakeFiles/tasks.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tasks.dir/main.c.o"
+  "CMakeFiles/tasks.dir/main.c.o.d"
+  "tasks"
+  "tasks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
